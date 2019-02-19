b959827cbe8d916b6d9ce1f1145aa371c15321f7
kummar <540496472@qq.com>
2019-02-19 15:20:19 +0800

    What add?

--- main.cpp
+++ main.cpp
@@ -4,42 +4,54 @@
 #include "Solution.h"
 #include <string>
 #include <stack>
+#include <cstdio>
 
 using namespace std;
 
-int main() {
-    string s = "{{([])}}";
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a=*b;
+    *b=t;
+}
 
-    stack<char> parentheses;
-
-    if (s.length() % 2 != 0)
+void partition(int A[], int left, int right, int *pos)
+{
+    int data=A[left];
+    int i;
+    for(*pos=left, i=left+1; i<=right; i++)
     {
-        return false;
-    }
-    // string s = "{{([])}}";
-    for(int i; i < s.size(); i++)
-    {
-        if(s[i] == '{' || s[i] == '(' || s[i] == '[')
+        if(A[i]<data)
         {
-            // 栈顶添加元素
-            parentheses.push(s[i]);
-            cout << "Push in parentheses is: " << s[i] << endl;
+            (*pos)++;
+            swap(&A[*pos], &A[i]);
         }
+    }
+    swap(&A[left], &A[*pos]);
+}
+
+int getMedian(int A[], int n)
+{
+    int left = 0;
+    int right = n - 1;
+    int mid = (left + right) / 2;
+    int pos, count = 1;
+    while (1)
+    {
+        partition(A,left,right,&pos);
+        if(pos==mid)
+            break;
+        else if(pos>mid)
+            right=pos-1;
         else
-        {
-            if(parentheses.empty()) return false;
-
-            // 返回栈顶元素
-            cout << "Check top or pop is: " << s[i] << " And top is: " << parentheses.top() << endl;
-            if(s[i] == '}' && parentheses.top() != '{') return false;
-            if(s[i] == ')' && parentheses.top() != '(') return false;
-            if(s[i] == ']' && parentheses.top() != '[') return false;
-
-            // 移除栈顶元素
-            cout << "Pop from parentheses is: " << s[i] << endl;
-            parentheses.pop();
-            cout << "Now stack info is: " << parentheses.top() << " Empty or not: " << parentheses.empty() << endl;
-        }
+            left=pos+1;
     }
-    return parentheses.empty();
+    return (n&0x1)!=0?A[mid]:(A[mid]+A[mid+1])/2;
+}
+
+int main() {
+    int Array[] = {7,1,3,9,11,14,16,2};
+    int mid;
+    mid = getMedian(Array, sizeof(Array) / sizeof(19));
+    cout << mid << endl;
 }
